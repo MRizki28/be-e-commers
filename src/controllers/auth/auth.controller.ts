20daa7ca978d42174a679d12830910e229f7b3d3
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-19 01:05:53 +0800

    feat(auth):create API auth

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/config/jwt/jwtAuth.guard';
+import { AuthDto } from 'src/dto/auth.dto';
+import { AuthService } from 'src/services/auth/auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ) {}
+
+    @Post('/login')
+    async login(@Body() authDto: AuthDto) {
+        return await this.authService.login(authDto);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('/logout')
+    async logout(@Req() req){
+        const userId = req.user.userId;
+        console.log('User ID:', userId);
+        return await this.authService.logout(userId);
+    }
+
+}
