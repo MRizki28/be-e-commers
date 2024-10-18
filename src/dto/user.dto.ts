51ff777b8f63e8225057ec2b135b599c23d71cfa
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-19 01:06:14 +0800

    feat(auth):create API auth

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,11 +1,15 @@
-import { isNotEmpty, IsNotEmpty, validate, Validate } from "class-validator";
-import { UniqueEmail } from "src/config/validator/UniqueEmail";
+import { IsNotEmpty, Length, Matches} from "class-validator";
 import { File } from "buffer";
-// import { idIsSame } from "src/config/validator/IdIsSame";
 
 export class UserDto {
     @IsNotEmpty()
     email: string;
     @IsNotEmpty()
+    @Length(5, 20, {
+        message: 'Password must be at least 5 characters long and at most 20 characters long.'
+    })
+    @Matches(/[A-Z]/, {
+        message: 'Password must contain at least one uppercase letter.'
+    })
     password: string;
 }
