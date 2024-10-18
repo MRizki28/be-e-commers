309121dfc806018ffa982755799887925b8d31bc
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-19 01:06:30 +0800

    feat(auth):create API auth

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,15 +5,7 @@ import { PrismaService } from 'src/services/prisma.service';
 import { UserService } from 'src/services/user/user.service';
 
 @Module({
-    imports: [
-        JwtModule.register({
-            global: true,
-            secret: process.env.JWT_SECRET,
-            signOptions: {
-                expiresIn: '1d'
-            }
-        })
-    ],
+    imports: [],
     controllers: [UserController],
     providers: [UserService, PrismaService],
 })
