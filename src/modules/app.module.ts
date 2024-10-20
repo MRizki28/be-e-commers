615fb1926ee8c3adef5e836b56ad65dbb9cf54c6
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-20 15:43:55 +0800

    feat(product): create product API

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from 'src/controllers/app.controller';
 import { AppService } from 'src/services/app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
