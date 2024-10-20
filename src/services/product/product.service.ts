e9e4c7c72f01fe1163a52809bea767fbf8fb62a6
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-20 17:52:23 +0800

    feat(product): new Update and delete API

--- src/services/product/product.service.ts
+++ src/services/product/product.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { HttpResponseTraits } from 'src/traits/HttpResponseTrait';
 import { ProductDto } from 'src/dto/product.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { createWriteStream } from 'fs';
+import * as fs from 'fs';
 
 @Injectable()
 export class ProductService {
@@ -147,6 +148,128 @@ export class ProductService {
         } catch (error) {
             console.log(error);
             if (error instanceof UnprocessableEntityException) {
+                throw error;
+            }
+        };
+    }
+
+    async getDataById(id: string): Promise<any> {
+        try {
+            const data = await this.prisma.product.findUnique({
+                where: {
+                    id: id
+                }
+            });
+
+            if (!data) {
+                HttpResponseTraits.dataNotFound();
+            }
+
+            return HttpResponseTraits.success(data, 'Success get data by id');
+        } catch (error) {
+            console.log(error);
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+        };
+    }
+
+    async updateData(id: string, productDto: ProductDto, productImg: Express.Multer.File): Promise<any> {
+        try {
+            const { name_product, stock, price, description } = productDto;
+            if (!productImg || !['image/jpeg', 'image/png'].includes(productImg.mimetype)) {
+                throw new UnprocessableEntityException({
+                    status: 'not validate',
+                    message: [
+                        {
+                            field: 'product_img',
+                            error: 'Product_img is required and must be a JPEG or PNG image',
+                        },
+                    ],
+                });
+            }
+
+            const data = await this.prisma.product.findUnique({
+                where: {
+                    id: id
+                }
+            })
+
+            if (!data) {
+                HttpResponseTraits.dataNotFound();
+            }
+
+            const filename = `${uuidv4()}-${productImg.originalname}`;
+            const path = `./public/uploads/product/${filename}`;
+            const writeStream = createWriteStream(path)
+            const oldFile = `./public/uploads/product/${data.product_img}`;
+
+            if (fs.existsSync(oldFile)) {
+                fs.unlinkSync(oldFile);
+            }
+
+            const fileUploadPromise = new Promise((resolve, reject) => {
+                writeStream.on('finish', () => resolve(true));
+                writeStream.on('error', (err) => reject(err));
+                writeStream.write(productImg.buffer);
+                writeStream.end();
+            })
+
+            await fileUploadPromise;
+
+            const updatedData = await this.prisma.product.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    name_product,
+                    stock: Number(stock),
+                    price: Number(price),
+                    description,
+                    product_img: filename
+                }
+            });
+
+            return HttpResponseTraits.success(updatedData, 'Success update product');
+        } catch (error) {
+            console.log(error);
+            if (error instanceof UnprocessableEntityException) {
+                throw error;
+            } else if (error instanceof NotFoundException) {
+                throw error;
+            }
+        };
+
+    }
+
+    async deleteData(id: string): Promise<any> {
+        try {
+            const data = await this.prisma.product.findUnique({
+                where: {
+                    id: id
+                }
+            });
+
+            if (!data) {
+                HttpResponseTraits.dataNotFound();
+            }
+
+            const file = `./public/uploads/product/${data.product_img}`;
+
+            if (fs.existsSync(file)) {
+                fs.unlinkSync(file);
+            }
+
+            await this.prisma.product.delete({
+                where: {
+                    id: id
+                }
+            });
+
+            return HttpResponseTraits.delete();
+        } catch (error) {
+            console.log(error);
+            if (error instanceof NotFoundException) {
                 throw error;
             }
         };
