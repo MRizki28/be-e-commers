34776b826cd7857929084b93fecc52329efa5bff
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-20 16:13:59 +0800

    feat(product): new create data API

--- src/controllers/product/product.controller.ts
+++ src/controllers/product/product.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ProductDto } from 'src/dto/product.dto';
 import { ProductService } from 'src/services/product/product.service';
 
 @Controller('product')
@@ -10,5 +12,11 @@ export class ProductController {
     @Get('/')
     async getAllData(@Query() req): Promise<any> {
         return this.productService.getAllData(req);
+    }
+
+    @Post('/create')
+    @UseInterceptors(FileInterceptor('product_img'))
+    async create(@Body() data: ProductDto, @UploadedFile() productImg: Express.Multer.File): Promise<any> {
+        return this.productService.createData(data, productImg);
     }
 }
