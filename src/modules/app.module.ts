70e3a6dab8d7060e70eabed60bc6f8bf2ffba82d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-21 14:01:32 +0800

    feat(authorization):add role access

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,11 +5,18 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { PrismaService } from 'src/services/prisma.service';
 
 
 @Module({
-  imports: [UserModule, AuthModule, ProductModule, CartModule],
+  imports: [UserModule, AuthModule, ProductModule, CartModule, OrderModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, {
+    provide: APP_GUARD,
+    useClass: RoleGuard
+  }],
 })
-export class AppModule {}
+export class AppModule { }
