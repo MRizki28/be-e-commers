fd600224b3ac493d5c5d667490346cc8f29c6d9c
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-23 12:34:10 +0800

    update

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/common/decorator/role.decorator';
+import { Role } from 'src/common/enums/role.enum';
 import { JwtAuthGuard } from 'src/config/jwt/jwtAuth.guard';
 import { UserDto } from 'src/dto/user.dto';
 import { UserService } from 'src/services/user/user.service';
@@ -9,8 +11,9 @@ export class UserController {
         private readonly userService: UserService,
     ) {}
 
-    // @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard)
     @Get('/')
+    @Roles(Role.ADMIN)
     async getAllData(@Query() req) {
         return await this.userService.getAllData(req);
     }
@@ -21,17 +24,23 @@ export class UserController {
         return await this.userService.createData(userDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/get/:id')
+    @Roles(Role.ADMIN)
     async getDataById(@Param('id') id: string): Promise<any> {
         return await this.userService.getDataById(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post('/update/:id')
+    @Roles(Role.ADMIN)
     async updateData(@Param('id') id: string, @Body() userDto: UserDto): Promise<any> {
         return await this.userService.updateData(id, userDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete('/delete/:id')
+    @Roles(Role.ADMIN)
     async deleteData(@Param('id') id: string): Promise<any> {
         return await this.userService.deleteData(id);
     }
